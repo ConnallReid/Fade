174b5a3b7a810d0f0d5f4431a3d9d1db163124b1
jamesfglasgow <Jamesfglasgow@gmail.com>
2017-05-18 23:20:59 +0100

    cam

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	public float smoothTime = 0.25f;
+	public Vector2 volocity;
+	private Transform moveCamera;
+
+	void Start () 
+	{
+		moveCamera = transform;
+	}
+
+	void Update () 
+	{
+		//follow the player at a delay for smooth follow
+		moveCamera.position = new Vector3 (Mathf.SmoothDamp (moveCamera.position.x, player.transform.position.x, ref volocity.x, smoothTime), moveCamera.position.y, moveCamera.position.z);
+		moveCamera.position = new Vector3 (moveCamera.position.x, Mathf.SmoothDamp (moveCamera.position.y, player.transform.position.y, ref volocity.y, smoothTime), moveCamera.position.z);
+
+
+	}
+}
